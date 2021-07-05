01e563969720659ec99cd63b1467720d486f7526
YuriSilenok <yuri.silenok@gmail.com>
2021-07-05 15:42:25 +0300

    я заведу эти автотесты

--- conftest.py
+++ conftest.py
@@ -12,16 +12,18 @@ def go_to_home(browser):
     # app.run(debug=True)
     # browser.get(START_LINK)
 
+
 @pytest.fixture(scope="session")
 def browser():
     """Open the browser once for all tests."""
     try:
+        app.run()
         options = webdriver.ChromeOptions()
         options.headless = True
         options.add_argument("--no-sandbox")
         executable_path = '/home/travis/chromedriver/chromedriver'
-        # bro = webdriver.Chrome(executable_path=os.path.join(os.getcwd(), 'chromedriver'), options=options)
-        bro = webdriver.Chrome(executable_path=executable_path, options=options)
+        bro = webdriver.Chrome(executable_path=os.path.join(os.getcwd(), 'chromedriver'), options=options)
+        # bro = webdriver.Chrome(executable_path=executable_path, options=options)
         bro.wait = 5
         bro.implicitly_wait(bro.wait)
         yield bro
