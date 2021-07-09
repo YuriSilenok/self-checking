74b714225b8d31b80f8206a675d5af128b476899
YuriSilenok <yuri.silenok@gmail.com>
2021-07-09 17:30:50 +0300

    очень сложный запрос

--- debug.py
+++ debug.py
@@ -15,20 +15,41 @@ for student_task in db.session.query(StudentTask).all():
                   review.student.user.id if review.student else review.teacher.user.id,
                   review.student.user.first_name if review.student else review.teacher.user.first_name,
                   review.review_status.name)
-print()
+
 
 query__ = db.session.query(Solving)
 query__ = query__.join(StudentTask, Solving.student_task_id == StudentTask.id)
 query__ = query__.join(Review, Review.solving_id == Solving.id, isouter=True)
-query__ = query__.where((StudentTask.student_task_status_id == 3) & (StudentTask.student_id != 3))
+query__ = query__.where(StudentTask.student_task_status_id == 3)
 query__ = query__.group_by(Solving.student_task_id)
 
-exists__ = db.session.query(Solving)
+
+exists__ = db.session.query(Solving, Solving.id)
 exists__ = exists__.join(StudentTask, Solving.student_task_id == StudentTask.id)
 exists__ = exists__.join(Review, Review.solving_id == Solving.id, isouter=True)
-exists__ = exists__.where((StudentTask.student_task_status_id == 3) & (Review.student_id == 4))
+exists__ = exists__.group_by(Solving.student_task_id)
+exists__ = exists__.having(Review.student_id == 7)
 
-# print(query__.where(~exists__.exists()))
 
-for solving in query__.where(~exists__.exists()).all():
-    print(solving)
+print('===')
+print(query__)
+print('===')
+for s in query__.all():
+    print(s)
+print('===')
+print(exists__)
+print('===')
+for s in exists__.all():
+    print(s)
+print('===')
+
+# SA = query__.subquery('SA')
+# SB = exists__.subquery('SB')
+
+# finish__ = db.session.query(SA).join(SB, SB.c.id == SA.c.id, isouter=True).where(SB.c.id.is_(None))
+finish__ = query__.filter(~exists__.exists())
+print(finish__)
+print('===')
+for s in finish__.all():
+    print(s)
+print('===')
