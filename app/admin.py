4d5a2c191d79f69951a885a6d94857403d5afb8a
yuri.silenok <yuri.silenok@gmail.com>
2021-12-25 00:50:35 +0300

    Оптимизировал архитектуру БД

--- app/admin.py
+++ app/admin.py
@@ -2,33 +2,16 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.models import User
 
-
-# class TeacherInline(admin.StackedInline):
-#     model = Teacher
-#     can_delete = False
-#     verbose_name_plural = 'teacher'
-#
-#
-# class TeacherUser(User):
-#     inlines = (TeacherInline,)
-
-
-# admin.site.unregister(User)
-# admin.site.register(User, Teacher)
-admin.site.register(Teacher)
-admin.site.register(Student)
-
-admin.site.register(Group)
-admin.site.register(StudentStatus)
-admin.site.register(StudentTaskStatus)
-admin.site.register(Departament)
+admin.site.register(UserStudentGroup)
+admin.site.register(StudentGroup)
+admin.site.register(StudentGroupDiscipline)
 admin.site.register(Discipline)
 admin.site.register(Theme)
 admin.site.register(Task)
+admin.site.register(Requirement)
 admin.site.register(StudentTask)
-admin.site.register(StudentDiscipline)
-admin.site.register(ReviewStatus)
+admin.site.register(StudentTaskStatus)
 admin.site.register(Solving)
 admin.site.register(Review)
-admin.site.register(Requirement)
+admin.site.register(ReviewStatus)
 admin.site.register(ReviewComment)
--- app/serializers.py
+++ app/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from app.models import Group
+from app.models import StudentGroup
 from rest_framework import viewsets
 from rest_framework import permissions
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Group
+        model = StudentGroup
         fields = ['id', 'name']
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
+    queryset = StudentGroup.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
